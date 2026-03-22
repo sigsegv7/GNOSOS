1243cef96b2a94370cdbef670b1c1f8067e84a3d
Ian Moffett <ian@mirocom.org>
2026-03-22 17:13:08 -0400

    boot/x86_64: loader: Allocate buffer for kernel binary
    Signed-off-by: Ian Moffett <ian@mirocom.org>

--- gnosos/boot/arch/x86_64/Loader.c
+++ gnosos/boot/arch/x86_64/Loader.c
@@ -18,11 +18,32 @@
 /* Length of kernel binary in bytes */
 static UINTN KernelLength = 0;
 
+/* Kernel binary base */
+static VOID *KernelBinBase = NULL;
+
+/*
+ * Print the kernel size in a human readable
+ * format
+ */
+static inline void
+PrintKernelSize(void)
+{
+    PRINT_INFO("Kernel loaded : ");
+    if (KernelLength >= UNIT_MIB) {
+        Print(L"%d MiB\n", KernelLength / UNIT_MIB);
+    } else if (KernelLength >= UNIT_KIB) {
+        Print(L"%d KiB\n", KernelLength / UNIT_KIB);
+    } else {
+        Print(L"%d bytes\n", KernelLength);
+    }
+}
+
 EFI_STATUS
 LoaderPrepare(void)
 {
     EFI_STATUS Status;
     EFI_FILE_HANDLE FileHandle;
+    UINTN BytesRead;
 
     /* Only run once */
     if (KernelLength > 0) {
@@ -36,5 +57,26 @@ LoaderPrepare(void)
     }
 
     KernelLength = FileServiceGetLength(FileHandle);
+    Status = gBS->AllocatePool(
+        EfiLoaderData,
+        KernelLength,
+        (VOID **)&KernelBinBase
+    );
+
+    if (EFI_ERROR(Status)) {
+        PRINT_FATAL("Failed to allocate memory for kernel bin\n");
+        FileServiceClose(FileHandle);
+        return Status;
+    }
+
+    BytesRead = FileServiceRead(FileHandle, KernelLength, KernelBinBase);
+    if (BytesRead == 0) {
+        FileServiceClose(FileHandle);
+        gBS->FreePool(KernelBinBase);
+        KernelBinBase = NULL;
+        return EFI_OUT_OF_RESOURCES;
+    }
+
+    PrintKernelSize();
     return EFI_SUCCESS;
 }
